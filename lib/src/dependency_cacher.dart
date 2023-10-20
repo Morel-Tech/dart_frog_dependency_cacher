2fefc8d0ef33051c949fc599c521085386691012
Marcus Twichel <developer@marcustwichel.com>
2023-10-20 11:17:28 -0600

    more changes

--- lib/src/dependency_cacher.dart
+++ lib/src/dependency_cacher.dart
@@ -13,10 +13,6 @@ typedef DependencyBuilder<T> = FutureOr<T> Function(
 /// A function that finds a caching key given a context
 typedef KeyFinder = FutureOr<String?> Function(RequestContext context);
 
-typedef _CachedDependencyBuilder<T> = FutureOr<T> Function(
-  RequestContext context,
-);
-
 final Map<String, dynamic> _cache = {};
 
 /// A [Middleware] that injects a dependency asynchronously and caches it for
@@ -28,25 +24,21 @@ Middleware futureProvider<T>(
   bool Function(T)? cacheValid,
 }) {
   return (handler) {
-    return (outerContext) async {
-      final dependencies =
-          outerContext.read<_DartFrogCachedDependencyBuilders>();
-      final saved = dependencies[T.toString()];
-      if (saved == null) {
-        if (shouldCache) {
-          dependencies[T.toString()] = (context) async {
-            final key = await keyFinder?.call(context);
-            return _asyncMemo<T>(
-              () => create(context, key: key),
-              key: key,
-              cacheValid: cacheValid,
-            );
-          };
-        } else {
-          dependencies[T.toString()] = (context) => create(context);
-        }
+    return (context) async {
+      if (shouldCache) {
+        context.provide<Future<T>>(() async {
+          final key = await keyFinder?.call(context);
+          return _asyncMemo<T>(
+            () => create(context, key: key),
+            key: key,
+            cacheValid: cacheValid,
+          );
+        });
+      } else {
+        context.provide<Future<T>>(() => Future.value(create(context)));
       }
-      return handler(outerContext);
+
+      return handler(context);
     };
   };
 }
@@ -73,28 +65,5 @@ extension RequestContextAsync on RequestContext {
   ///
   /// An [Exception] is thrown if [T] is not available within
   /// the provided [request] context.
-  Future<T> readAsync<T>() async {
-    final dependencies = read<_DartFrogCachedDependencyBuilders>();
-    final depBuilder = dependencies[T.toString()];
-    if (depBuilder == null) {
-      throw Exception('Missing create function for type $T');
-    }
-    final dep = await depBuilder(this);
-    if (dep is T) {
-      return dep;
-    }
-    throw Exception(
-      'Dependency for $T was type ${dep.runtimeType.runtimeType}. '
-      'Expected $T',
-    );
-  }
-}
-
-class _DartFrogCachedDependencyBuilders {
-  final Map<String, _CachedDependencyBuilder<dynamic>> _dependencyBuilders = {};
-
-  _CachedDependencyBuilder<dynamic>? operator [](String key) =>
-      _dependencyBuilders[key];
-  void operator []=(String key, _CachedDependencyBuilder<dynamic> builder) =>
-      _dependencyBuilders[key] = builder;
+  Future<T> readAsync<T>() => read<Future<T>>();
 }
