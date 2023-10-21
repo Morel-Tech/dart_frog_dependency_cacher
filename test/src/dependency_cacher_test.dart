129881f03ff096c8d76dfde1348f29281fa9b14e
Marcus Twichel <developer@marcustwichel.com>
2023-10-20 20:51:34 -0600

    fix tests

--- test/src/dependency_cacher_test.dart
+++ test/src/dependency_cacher_test.dart
@@ -1,7 +1,32 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_dependency_cacher/dart_frog_dependency_cacher.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
+class _MockRequest extends Mock implements Request {}
+
+class _TestRequestContext implements RequestContext {
+  final dependencies = <String, dynamic>{};
+  @override
+  RequestContext provide<T extends Object?>(T Function() create) {
+    dependencies['$T'] = create();
+    return this;
+  }
+
+  @override
+  T read<T>() {
+    return dependencies['$T'] as T;
+  }
+
+  @override
+  Request get request => _MockRequest();
+}
+
 void main() {
-  test('noop', () {
-    expect(true, true);
+  test('readAsync calls read on Future of type', () {
+    final context = _TestRequestContext();
+    final newContext = context.provide(() => Future.value('__value__'));
+
+    expect(newContext.readAsync<String>(), completion('__value__'));
   });
 }
